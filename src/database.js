40b0f21fc684e38a70c5b8413854736f155d1872
zxcome <gop.niga228@gmail.com>
2026-03-15 15:24:33 +0200

    db2

--- src/database.js
+++ src/database.js
@@ -268,8 +268,10 @@ export const getUnverifiedUsers = () =>
 export const getUserByUsername = (username) =>
   db.prepare('SELECT * FROM users WHERE LOWER(username) = LOWER(?)').get(username.replace('@', ''));
 
-export const deleteSite = (id) =>
+export const deleteSite = (id) => {
+  db.prepare('DELETE FROM registrations WHERE site_id = ?').run(id);
   db.prepare('DELETE FROM sites WHERE id = ?').run(id);
+};
 
 export const getReferralEarnings = () =>
   db.prepare(`
