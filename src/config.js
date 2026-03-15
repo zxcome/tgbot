0ef04fa81d20c7b8bbae88f1f649167d5f211f88
zxcome <gop.niga228@gmail.com>
2026-03-15 17:45:52 +0200

    norm

--- src/config.js
+++ src/config.js
@@ -6,4 +6,4 @@ export const ADMIN_IDS = process.env.ADMIN_IDS
   : [];
 export const MANAGER_USERNAME = process.env.MANAGER_USERNAME || '@manager';
 export const CHANNEL_URL = process.env.CHANNEL_URL || 'https://t.me/yourchannel';
-export const DB_PATH = './bot.db';
+export const DB_PATH = process.env.DB_PATH || './bot.db';
